7ad66242530a01331feef607733df4eadfceed0f
Seth Grief-Albert <sethgriefalbert@gmail.com>
2023-06-08 20:43:28 -0400

    Add styles

--- App.js
+++ App.js
@@ -26,9 +26,25 @@ export default class App extends React.Component {
   }
 
   render () {
+    let textColor = 'black'; // zero black
+    if (this.state.value < 0) { // negative red
+      textColor = 'red';
+    } else if (this.state.value > 0) { // positive green
+      textColor = 'green';
+    }
+
+    let hiddenColor1 = 'white'; // hidden white 1
+    let hiddenColor2 = 'white'; // hidden white 2
+    if (this.state.value >= 10) { // revealed red
+      hiddenColor1 = 'red';
+    }
+    if (this.state.value >= 13) { // revealed red
+      hiddenColor2 = 'red';
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: 60, marginBottom: -20}}>{this.state.value}</Text>
+        <Text style={{ fontSize: 60, marginBottom: -20, color: textColor}}>{this.state.value}</Text>
         <Text style={{ fontSize: 12, padding: 20, color: 'grey'}}>{"Total taps: "+ this.state.total_taps}</Text>
         <StatusBar style="auto" />
         <View style={{flexDirection:'row'}}>
@@ -37,6 +53,11 @@ export default class App extends React.Component {
           <Button onPress={this.incrementValue} title="Increase" />
           <Text> </Text>
         </View>
+        <Text style={{ fontSize: 20, color: hiddenColor1}}>{"ERROR: THRESHOLD SURPASSED"}</Text>
+        <Text> </Text>
+        <Text style={{ fontSize: 15, color: hiddenColor2}}>{"Target aquired."}</Text>
+        <Text> </Text>
+        <Text style={{ fontSize: 15, color: hiddenColor2}}>{"5 MINUTES TO IMPACT"}</Text>
       </View>
     );
   }
